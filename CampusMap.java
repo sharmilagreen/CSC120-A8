b2e59bdd76c1f7ce0bf0240f6fb760fbf9310447
sharmilagreen <sagreen@smith.edu>
2025-05-05 23:02:07 -0400

    main

--- CampusMap.java
+++ CampusMap.java
@@ -44,6 +44,16 @@ public class CampusMap {
         CampusMap myMap = new CampusMap();
         myMap.addBuilding(new Building("Ford Hall", "100 Green Street Northampton, MA 01063", 4));
         myMap.addBuilding(new Building("Bass Hall", "4 Tyler Court Northampton, MA 01063", 4));
+        myMap.addBuilding(new House("Hubbard House", "1 Sharmila St", 3));
+        myMap.addBuilding(new Library("Neilson Library", "7 Maya Dr", 5, true));
+        myMap.addBuilding(new Cafe("Campus Cafe", "10 Sharmila St", 1));
+        myMap.addBuilding(new House("Chase House", "2 Jordan Ln"));
+        myMap.addBuilding(new Library("Hillyer Library", "12 Jordan Ln", 3, false));
+        myMap.addBuilding(new Cafe("Compass Cafe", "8 Sharmila St", 1));
+        myMap.addBuilding(new House("Morrow House", "3 Sharmila St", 4));
+        myMap.addBuilding(new Library("Science Library", "15 Kayla Rd", 6, true));
+        myMap.addBuilding(new Cafe("Blue Cafe", "17 Kayla Rd", 2));
+        myMap.addBuilding(new House("Park House", "19 Buddy Ave", 3));
         System.out.println(myMap);
     }
     
